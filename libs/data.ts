4810ed8dd2285008a7ca7ccfff5f3e4222a021db
zeus012345 <francisjerry360@gmail.com>
2022-11-03 22:55:38 +0100

    new

--- libs/data.ts
+++ libs/data.ts
@@ -2,7 +2,7 @@ import product1 from "../assets/product1.png";
 import product2 from "../assets/product2.png";
 import product3 from "../assets/product3.png";
 import product4 from "../assets/product4.png";
-import product5 from "../assets/product5.png";
+import product5 from "../assets/product6.png";
 
 export interface ProductProps {
   women: {
